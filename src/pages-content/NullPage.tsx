a65f5d28dcc0afa79741bd9e6c766c713de5eb47
guimamaral <guim.amaral2004@gmail.com>
2026-02-25 02:35:17 +0000

    hehe

--- src/pages-content/NullPage.tsx
+++ src/pages-content/NullPage.tsx
@@ -2,9 +2,26 @@
 
 import styles from './pages.module.css';
 
+const REGS = `\
+ rip: 0x0000000000000000   rsp: 0x00007fff5fbff8a0
+ rax: 0x00000000deadbeef   rbx: 0x0000000000000000
+ rcx: 0x00007f4a2b1c0000   rdx: 0x0000000000000000
+ rsi: 0x0000000000000001   rdi: 0x0000000000000000
+ rbp: 0x00007fff5fbff8c0    r8: 0x0000000000000000
+  r9: 0x00007f4a2b0fffff   r10: 0x0000000000000246
+ r11: 0x0000000000000246   r12: 0x00000000cafebabe
+ r13: 0xffffffffffffffff   r14: 0x00007f4a2b1c4000
+ r15: 0x0000000000000000`;
+
 export function NullPage() {
   return (
     <section className={styles.nullPage}>
+      <div className={styles.crashDump}>
+        <span className={styles.crashSignal}>*** SIGSEGV (Segmentation fault) received ***</span>
+        <pre className={styles.crashRegs}>{REGS}</pre>
+        <span className={styles.crashFault}>Segmentation fault (core dumped)</span>
+      </div>
+
       <p className={styles.nullIntro}>
         You have just dereferenced a pointer with value NULL. Your reward is the following knowledge bite from renowned British computer scientist, Sir Tony Hoare.
       </p>
